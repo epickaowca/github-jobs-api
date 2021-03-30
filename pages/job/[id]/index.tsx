e78e68dd99e301f1fbe768591f10913f5e51757d
epickaowca <owcaofficial@yahoo.com>
2021-03-30 19:35:52 +0200

    test

--- pages/job/[id]/index.tsx
+++ pages/job/[id]/index.tsx
@@ -9,26 +9,26 @@ const Job = ({article}) => {
 }
 
 
-export const getStaticProps = async(context)=>{
-    const res = await axios(`http://localhost:3000/api/selectedJob?id=${context.params.id}`)
-    const article = res.data
-    return{
-        props:{
-            article,
-        }
-    }
-}
+// export const getStaticProps = async(context)=>{
+//     const res = await axios(`http://localhost:3000/api/selectedJob?id=${context.params.id}`)
+//     const article = res.data
+//     return{
+//         props:{
+//             article,
+//         }
+//     }
+// }
 
-export const getStaticPaths = async ()=>{
-    const res = await axios('http://localhost:3000/api/allJobs')
-    const articles = res.data
-    const ids = articles.map(article => article.id)
-    const paths = ids.map(id => ({params: {id: id.toString()}}))
-    return{
-        paths: paths,
-        fallback: false,
-    }
-}
+// export const getStaticPaths = async ()=>{
+//     const res = await axios('http://localhost:3000/api/allJobs')
+//     const articles = res.data
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+//     return{
+//         paths: paths,
+//         fallback: false,
+//     }
+// }
 
 
 export default Job
